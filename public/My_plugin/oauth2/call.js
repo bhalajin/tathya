c79dd002c02fd23098f2ab775500946da2273129
sanjivkrish <sanjiv436.k@gmail.com>
2016-04-23 17:35:19 +0530

    plugin: Inject Oauth2 dependency for user login
    Signed-off-by: sanjivkrish <sanjiv436.k@gmail.com>

---
+++ public/My_plugin/oauth2/call.js
@@ -0,0 +1,4 @@
+        window.focus();
+        window.setTimeout( function () {var redirect = "http://www.google.com/robots.txt/close";
+        window.location = redirect;
+                                       },2000);
